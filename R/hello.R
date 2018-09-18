5592f67fc1d2155d029d3c932a285fb7e691e5d3
tcgriffith <ztc.ustcer@gmail.com>
2018-09-18 16:32:28 +1000

    jobs... steve jobs

---
+++ R/hello.R
@@ -0,0 +1,16 @@
+#' hello world
+#'
+#' 你好，世界
+#'
+#' 中文是OK的
+#'
+#' @return nothing
+#' @export
+#'
+#'
+#'
+#' @examples
+#' hello()
+hello <- function() {
+  print("Hello, world!")
+}
---
+++ Readme.md
@@ -0,0 +1,11 @@
+
+
+- build:
+
+`R CMD build sotenbori`
+
+- check:
+
+```
+R CMD check sotenbori_0.1.0.tar.gz --as-cran
+```
